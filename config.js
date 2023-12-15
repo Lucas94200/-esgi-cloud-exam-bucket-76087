27f587638462e22f0473fd01e7ceab5c8e635176
Lucas Yao <114393993+Lucas94200@users.noreply.github.com>
2023-12-15 11:23:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'UAXAOUPIOODA53QWJVBF', // TODO
+    secretAccessKey: 'NO2bLBQU6bGTy7vHbfa2CPe6MoyjOLvumg2GSBSl', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-76087', // TODO
   },
   suffix: {
     small: '_small.jpg',
